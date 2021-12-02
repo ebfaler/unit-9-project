48ef2a16d46a3fe4c38c1442e804e127d38eec81
ebfaler <74653573+ebfaler@users.noreply.github.com>
2021-12-02 23:42:49 +0100

    version with img opacity

--- js/app.js
+++ js/app.js
@@ -1,9 +1,40 @@
-// source: https://codepen.io/mondal10/pen/WNNEvjV
+source: https://codepen.io/mondal10/pen/WNNEvjV
+
+// var cards = document.querySelectorAll('.card');
+
+// cards.forEach((card)=>{
+//   card.addEventListener( 'click', function() {
+//     card.classList.toggle('is-flipped');
+//   });
+// });
+
 
 var cards = document.querySelectorAll('.card');
+var viewMore = document.querySelectorAll('.view-more');
 
-[...cards].forEach((card)=>{
-  card.addEventListener( 'click', function() {
-    card.classList.toggle('is-flipped');
-  });
-});
+
+cards.forEach((card) =>
+
+  card.addEventListener('click', e => {
+    if (e.target.classList == "view-more") {
+      console.log("card flipped");
+      card.classList.toggle('is-flipped');
+    }
+  })
+);
+
+// cards.forEach((card) => {
+//   const viewMore = card.querySelector('.view-more');
+//   const viewProject = card.querySelector('.view-project');
+
+//   viewMore.addEventListener('click', e => {
+//     card.classList.toggle('is-flipped');
+//   });
+//   viewProject.addEventListener('click', e => {
+//     console.log("view project");
+//   });
+// });
+
+
+
+
