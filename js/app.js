12259df260f704f42e35a29dd233b0e6a9023a34
ebfaler <74653573+ebfaler@users.noreply.github.com>
2021-12-06 16:43:00 +0100

    refactored event listeners

--- js/app.js
+++ js/app.js
@@ -6,30 +6,48 @@ let cards = document.querySelectorAll('.card');
 let viewMore = document.querySelectorAll('.view-more');
 let cardFace = document.querySelectorAll('.card__face');
 
-
-
 cards.forEach((card) => {
   card.addEventListener('click', e => {
     if (e.target.classList == "view-more") {
       console.log("card flipped");
-      if (card.classList.contains('is-flipped')) {
-        card.classList.remove('is-flipped');
-      } else {
-        card.classList.add('is-flipped');
-      }
 
-    }
+      card.classList.add('is-flipped');
+      }
   });
 
   card.addEventListener('click', e => {
     if (e.target.classList.contains("card__face")) {
-      console.log("card clicked");
+      console.log("card flipped back ");
       card.classList.remove('is-flipped');
     }
   })
 
 }
 );
+
+
+// cards.forEach((card) => {
+//   card.addEventListener('click', e => {
+//     if (e.target.classList == "view-more") {
+//       console.log("card flipped");
+//       if (card.classList.contains('is-flipped')) {
+//         card.classList.remove('is-flipped');
+//       } else {
+//         card.classList.add('is-flipped');
+//       }
+
+//     }
+//   });
+
+//   card.addEventListener('click', e => {
+//     if (e.target.classList.contains("card__face")) {
+//       console.log("card clicked");
+//       card.classList.remove('is-flipped');
+//     }
+//   })
+
+// }
+// );
 
 
 
